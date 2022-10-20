e187ec79fa1be3b219558b601ff4897dd848d0db
mahdikhalil <mahdikhalil@esprit.tn>
2022-10-20 15:32:30 +0200

    change credantials

--- src/app/change-credentials/change-credentials.component.ts
+++ src/app/change-credentials/change-credentials.component.ts
@@ -2,28 +2,38 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {AuthService} from "../services/AuthService";
 import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-change-credentials',
   templateUrl: './change-credentials.component.html',
   styleUrls: ['./change-credentials.component.css']
 })
-export class ChangeCredentialsComponent implements OnInit,OnDestroy {
+export class ChangeCredentialsComponent implements OnInit, OnDestroy {
 
   subscriptions: Subscription = new Subscription();
 
-  email: string;
-  password: string;
-  errorMessage = 'Cette adresse mail est déja prise';
+  email: string = "";
+  password: string = "";
+  errorMessage = 'Cette adresse mail est prise';
   successMessage: string;
   invalidChange = false;
   changeSuccess = false;
   show: boolean = false;
+  showConfirm: boolean = false;
+  Confirmpassword: string = "";
 
-  constructor(private authService : AuthService,
-              private router: Router,) { }
+  constructor(private authService: AuthService,
+              private router: Router,
+              private toaster: ToastrService,) {
+  }
 
   ngOnInit(): void {
+    this.subscriptions.add(this.authService.userLoggedIn$.subscribe(user => {
+      if (user == false) {
+        this.router.navigate(['/projects']);
+      }
+    }));
   }
 
   ngOnDestroy(): void {
@@ -31,15 +41,21 @@ export class ChangeCredentialsComponent implements OnInit,OnDestroy {
   }
 
   changeCredentials() {
-    this.subscriptions.add(this.authService.userLoggedIn$.subscribe(user =>{
-      console.log(user);
-      this.subscriptions.add(this.authService.changeCredentials(null,this.email,this.password).subscribe(next =>{},response=>{
-        if(response.status == 200){
-          this.changeSuccess = true;
-        }else{
-          this.invalidChange = true;
-        }
-      }));
+    this.subscriptions.add(this.authService.userLoggedIn$.subscribe(user => {
+      if(this.Confirmpassword !== this.password){
+        this.toaster.error("Vérifiez votre nouveau mot de passe");
+      }else{
+        this.subscriptions.add(this.authService.changeCredentials(user, this.email != "" ? this.email : "null", this.password != "" ? this.password : "null").subscribe(next => {
+        }, response => {
+          if (response.status == 200) {
+            this.toaster.success("Modification sauvegarder");
+            this.changeSuccess = true;
+            this.logout();
+          } else {
+            this.invalidChange = true;
+          }
+        }));
+      }
     }));
   }
 
@@ -48,10 +64,14 @@ export class ChangeCredentialsComponent implements OnInit,OnDestroy {
   }
 
   logout() {
-      this.authService.disconnet();
+    this.authService.disconnet();
   }
 
   showHide() {
     this.show = !this.show;
+  }
+
+  showConfirmHide() {
+    this.showConfirm = !this.showConfirm;
   }
 }
